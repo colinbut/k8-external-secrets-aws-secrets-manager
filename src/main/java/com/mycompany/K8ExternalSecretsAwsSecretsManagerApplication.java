0f55d715badcb4f42443ea542f20d440ffc01300
Colin But <colin.but@outlook.com>
2021-12-23 18:19:29 +0000

    initial commit

---
+++ src/main/java/com/mycompany/K8ExternalSecretsAwsSecretsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8ExternalSecretsAwsSecretsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8ExternalSecretsAwsSecretsManagerApplication.class, args);
+	}
+
+}
